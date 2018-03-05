4411cecdc0f3d23512b0710ea8494aba41cff5ad
16CO109 <darshan.blh@gmail.com>
2018-03-05 19:29:47 +0530

    All the pages with the Miwok and English words are completed.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,4 +7,20 @@ package com.example.android.miwok;
 public class Word {
 
     //private
+    private String mDefaultTranslation,mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation)
+    {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getDefaultTranslation()
+    {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation(){
+        return mMiwokTranslation;
+    }
 }

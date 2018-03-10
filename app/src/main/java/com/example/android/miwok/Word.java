e5399cf68be808daf603a64155762d523ff40f0f
16CO109 <darshan.blh@gmail.com>
2018-03-10 21:48:43 +0530

    Adding of images complete

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,11 +8,24 @@ public class Word {
 
     //private
     private String mDefaultTranslation,mMiwokTranslation;
+    private int mImageResourceID ;
 
     public Word(String defaultTranslation, String miwokTranslation)
     {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
+
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceID = imageResourceID;
+    }
+
+    public int getImageResourceID() {
+
+        return mImageResourceID;
     }
 
     public String getDefaultTranslation()
